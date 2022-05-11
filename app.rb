9c1ee9011d95c2d640fac795aad017fc4e891164
Jelani Woods <jelani@firstdraft.com>
2022-05-11 08:52:45 -0500

    Refactor test output capture

--- app.rb
+++ app.rb
@@ -2,44 +2,43 @@ require "json"
 require "active_record"
 require "sqlite3"
 require "fileutils"
-# Require models
-Dir[File.join(__dir__, "models", "*.rb")].each { |file| require_relative file }
 
 # event:
 # {
-#   "payload": 
-#     { 
+#   "payload":
+#     {
 #       "query": "Director.first.movies",
 #       "database": "msm",
 #       "level": "one",
 #       "specs": [
 #         {
 #          "name": "uuid",
-#           "body": "it \"should do include a Movie from the first Director\" do               expect(results).to include Movie.find_by(director_id: 1)             end"
+#           "body": "it \"should do include a Movie from the first Director\" do\n    expect(results).to include Movie.find_by(director_id: 1)\n  end"
+#         }
+#       ],
+#       "models": [
+#         {
+#           "name": "movie.rb",
+#           "body": "class Movie < ActiveRecord::Base\n belongs_to :director\n has_many :characters\n end"
 #         }
 #       ]
 #     }
 #   }
 def lambda_handler(event:, context:)
-  payload = event["payload"]
+  # TODO: Handle this gracefully
+  payload = event.fetch("payload")
   keys = %w{query database level specs models}
   query, database, level, specs, models = payload.values_at(*keys)
 
-  # It appears I need to freshly copy the db to the /tmp/ folder at runtime
-  # The file from the image doesn't register
-  FileUtils.cp("#{database}.sqlite3", "/tmp/")
-  ActiveRecord::Base.establish_connection(
-    adapter: "sqlite3",
-    database: "/tmp/#{database}.sqlite3",
-  )
+  connect_to_db(database)
 
   # Create spec and models folders
-  Dir.mkdir('/tmp/spec') #unless Dir.exist?('/tmp/spec')
-  Dir.mkdir('/tmp/models')# unless Dir.exist?('/tmp/models/')
+  Dir.mkdir("/tmp/spec") unless Dir.exist?('/tmp/spec')
+  Dir.mkdir("/tmp/models") unless Dir.exist?('/tmp/models/')
   write_spec_helper
+
   specs.each do |spec|
-    filename = spec["name"]
-    body = spec["body"]
+    filename, body = spec.values_at("name", "body")
     write_spec(filename, body)
   end
 
@@ -47,17 +46,23 @@ def lambda_handler(event:, context:)
     filename, body = model.values_at("name", "body")
     write_model(filename, body)
   end
-  result = eval(query)
-  # minitest_output = `QUERY='#{query}' ruby test/level_#{level}_tests.rb`
-  puts rspec_test_output = `QUERY='#{query}' bundle exec rspec /tmp/spec/ --format j`
 
+  Dir[File.join("/tmp", "models", "*.rb")].each do |file|
+    require_relative file
+  end
+
+  result = eval(query)
+  # minitest_output = minitest_output(query)
+  rspec_test_output = rspec_output(query)
+
+  # TODO maybe extract this as well
   {
     statusCode: 200,
     body: {
       query: query,
       return_value: result,
-      # minitest_results: JSON.parse(minitest_output),
-      rspec_test_results: JSON.parse(rspec_test_output)
+      # minitest_output: JSON.parse(minitest_output),
+      rspec_test_output: JSON.parse(rspec_test_output),
     }.to_json,
   }
 end
@@ -66,23 +71,24 @@ def write_spec(filename, body)
   filename = "/tmp/spec/#{filename}_spec.rb"
   tmp_file = File.open(filename, "a")
   tmp_file.seek(0)
-  tmp_file.write("require_relative './spec_helper.rb'\n")
-  connect_to_db = <<-RUBY
-describe "Level one" do
-  before do
-    ActiveRecord::Base.establish_connection(
-      adapter: "sqlite3",
-      database: File.join("/tmp", "msm.sqlite3")
-    )
-  end
-
+  # TODO organize with level name
+  # TODO use specified database
+  content = <<~RUBY
+    require_relative './spec_helper.rb'
+    describe "Level one" do
+      before do
+        ActiveRecord::Base.establish_connection(
+          adapter: "sqlite3",
+          database: File.join("/tmp", "msm.sqlite3")
+        )
+      end
+      query = ENV['QUERY']
+      let(:results) { eval(query) }
+      #{body}
+    end
   RUBY
 
-  tmp_file.write(connect_to_db)
-  tmp_file.write(eval_query)
-  tmp_file.write(body)
-  # close the "describe" block
-  tmp_file.write("\nend")
+  tmp_file.write(content)
   tmp_file.close
   filename
 end
@@ -96,25 +102,48 @@ def write_model(filename, body)
   filename
 end
 
-def eval_query
-  # TODO make sure multi-line queries work
-  <<-RUBY
-  query = ENV['QUERY']
-  let(:results) { eval(query) }
-  RUBY
+def minitest_output(query)
+  # `QUERY='#{query}' ruby test/level_#{level}_tests.rb`
+end
+
+def rspec_output(query)
+  `QUERY='#{query}' bundle exec rspec /tmp/spec/ --format j`
 end
 
 def write_spec_helper
+  # TODO: load models from /tmp/
   content = <<~RUBY
-  require 'active_record'
-  require 'sqlite3'
-  Dir[File.join("/var", "task", "models", "*.rb")].each do
-    |file| require_relative file
-  end
+    require 'active_record'
+    require 'sqlite3'
+    Dir[File.join("/tmp", "models", "*.rb")].each do
+      |file| require_relative file
+    end
+    # ActiveRecord::Base.establish_connection(
+    #   adapter: "sqlite3",
+    #   database: File.join("/tmp", "msm.sqlite3")
+    # )
   RUBY
   filename = "/tmp/spec/spec_helper.rb"
   tmp_file = File.open(filename, "a")
   tmp_file.seek(0)
   tmp_file.write(content)
   tmp_file.close
+end
+
+def render_error(message)
+  {
+    statusCode: 500,
+    error: message,
+  }.to_json
+end
+
+def connect_to_db(database)
+  # It appears I need to freshly copy the db to the /tmp/ folder at runtime
+  FileUtils.cp("#{database}.sqlite3", "/tmp/")
+  # TODO Why is this required in the function?
+  # shouldn't it just need to be in the spec file?
+  ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "/tmp/#{database}.sqlite3",
+  )
 end
