d343c97a07983052fe8765154b81875c49e52144
Jelani Woods <jelani@firstdraft.com>
2022-08-10 15:06:27 -0500

    WIP

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ gem "minitest"
 gem "minitest-reporters-json_reporter"
 gem "rspec"
 ruby '~> 2.7.0'
+
+gem "awesome_print"
--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "json"
 require "active_record"
 require "sqlite3"
 require "fileutils"
+require "awesome_print"
 
 # event:
 # {
@@ -26,30 +27,54 @@ require "fileutils"
 #   }
 def lambda_handler(event:, context:)
   # TODO: Handle this gracefully
+  # TODO: if there's an error, it seems to just respond with "" until server restarts- Why?
   payload = event.fetch("payload")
+  ap payload
   keys = %w{query database level specs models}
   query, database, level, specs, models = payload.values_at(*keys)
 
   recreate_directories
   connect_to_db(database)
-
+  puts "Connected to DB\n\n"
+  puts "Writing spec helper"
   write_spec_helper
 
+  puts "Writing specs..."
   specs.each do |spec|
     filename, body = spec.values_at("name", "body")
     write_spec(filename, body)
   end
 
+  puts "Writing models..."
   models.each do |model|
     filename, body = model.values_at("name", "body")
     write_model(filename, body)
   end
 
+  puts "Loading models... #{ Dir[File.join("/tmp", "models", "*.rb")].length}"
   Dir[File.join("/tmp", "models", "*.rb")].each do |file|
     require_relative file
   end
+  puts "======\n\n"
+  Dir[File.join("/tmp", "models", "*.rb")].each do |file|
+    p file
+    puts "----"
+    puts open(file).read
+    puts "_____"*3
+  end
 
-  result = eval(query)
+  puts "evaluating query..."
+  result = ""
+  begin
+    result = eval(query)
+
+  rescue => exception
+    result = exception.message
+  end
+  return_class = result.class.to_s
+  if result.class.ancestors.include?(ActiveRecord::Base)
+    return_class += "::ActiveRecord::Base"
+  end
   # minitest_output = minitest_output(query)
   rspec_test_output = rspec_output(query)
 
@@ -58,6 +83,7 @@ def lambda_handler(event:, context:)
     body: {
       query: query,
       return_value: result,
+      return_class: return_class,
       # minitest_output: JSON.parse(minitest_output),
       rspec_test_output: JSON.parse(rspec_test_output),
     }.to_json,
@@ -96,6 +122,9 @@ def write_model(filename, body)
   tmp_file.seek(0)
   tmp_file.write(body)
   tmp_file.close
+  puts filename
+  puts body
+  puts "---------\n\n"
   filename
 end
 
@@ -112,8 +141,8 @@ def write_spec_helper
   content = <<~RUBY
     require 'active_record'
     require 'sqlite3'
-    Dir[File.join("/tmp", "models", "*.rb")].each do
-      |file| require_relative file
+    Dir[File.join("/tmp", "models", "*.rb")].each do |file|
+      require_relative file
     end
     # ActiveRecord::Base.establish_connection(
     #   adapter: "sqlite3",
@@ -146,8 +175,19 @@ def connect_to_db(database)
 end
 
 def recreate_directories
+  puts "recreatings..."
+  Dir[File.join("/tmp", "models", "*.rb")].each do |file|
+    puts file
+    puts open(file).read
+  end
+
   FileUtils.rm_rf("/tmp/spec")
   FileUtils.rm_rf("/tmp/models")
   Dir.mkdir("/tmp/spec")
   Dir.mkdir("/tmp/models")
+  puts "should be empty"
+  Dir[File.join("/tmp", "models", "*.rb")].each do |file|
+    puts file
+    puts open(file).read
+  end
 end
